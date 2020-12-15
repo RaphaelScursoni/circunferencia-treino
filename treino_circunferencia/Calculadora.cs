a30e7136b33bccd9d39aec6539fcc45bb885d7a0
Raphael Scursoni <ra.scursoni@gmail.com>
2020-12-15 10:27:09 -0300

    término do cálculo de circunferência

---
+++ treino_circunferencia/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace treino_circunferencia {
+    class Calculadora {
+
+        public static double iof = 6.0;
+
+        public static double ConversorDeMoeda(double d, double q) {
+            double total = d * q;
+            return total + total * iof / 100;
+        }
+    }
+}
---
+++ treino_circunferencia/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Transactions;
+
+namespace treino_circunferencia {
+    class Program {
+        static void Main(string[] args) {
+
+            double dolar;
+            int quantidade;
+
+            Calculadora calc = new Calculadora();
+
+            Console.Write("Digite o valor do dólar: ");
+            dolar = double.Parse(Console.ReadLine());
+            Console.Write("Quantos dolares irá comprar?: ");
+            quantidade = int.Parse(Console.ReadLine());
+
+            double valor = Calculadora.ConversorDeMoeda(dolar, quantidade);
+
+            Console.WriteLine("Valor à ser pago em reais: " + valor.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
